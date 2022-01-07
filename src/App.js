04424097100336528c952fb9d7c50629756d5f11
Pingcube <pingcube@Pingcubes-MacBook-Air.local>
2022-01-08 04:05:37 +0530

    changed Home to Side Nav

--- src/App.js
+++ src/App.js
@@ -1,103 +1,66 @@
-
-
-
 import * as React from "react";
 
-import { Link,BrowserRouter, Routes, Route,useNavigate } from "react-router-dom";
-import {AuthProvider} from './hooks/useAuth'
-import useAuth from './hooks/useAuth'
-import {RequireAuth} from './RequiredAuth'
-import Login from './pages/Login'
-import Home from './pages/Home'
+import {
+  Link,
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import { AuthProvider } from "./hooks/useAuth";
+import useAuth from "./hooks/useAuth";
+import { RequireAuth } from "./RequiredAuth";
+import Login from "./pages/Login";
+import SideNav from "./pages/SideNav";
 import { createChatData } from "./data";
 import Chat from "./pages/Chat";
-import {Box,Flex} from 'rebass'
+import { Box, Flex } from "rebass";
 import { useWindowHeight } from "@react-hook/window-size";
 
-// const Home = () => <h1>Home (Public)</h1>;
-const Pricing = () => <h1>Pricing (Public)</h1>;
-
-const Dashboard = () => <h1>Dashboard (Private)</h1>;
-const Settings = () => <h1>Settings (Private)</h1>;
-
-
-
-
-// function Nav() {
-//   const { authed,data, logout } = useAuth();
-//   const navigate = useNavigate();
-//   const handleLogout = () => {
-//     logout();
-//     navigate("/");
-//   };
-
-//   return (
-//     <nav>
-//       <ul>
-//         <li>
-//           <Link to="/">Home</Link>
-//         </li>
-//       </ul>
-//       {authed && (
-//         <button onClick={handleLogout}>
-//           Logout
-//         </button>
-//       )}
-//     </nav>
-//   );
-// }
-
-
-
 function App() {
-
-  const height = useWindowHeight()
-
-  // const [data,setData] = React.useState([])
-
-  // React.useEffect(()=>{
-  //   const getData= async()=>{
-  //    const createData = await createChatData()
-  //    setData(createData)
-  //   }
-  // getData()
-  // },[])
+  const height = useWindowHeight();
 
   return (
     <AuthProvider>
-   <BrowserRouter>
-    <div className="App">
-      {/* <Nav /> */}
+      <BrowserRouter>
+        <div className="App">
+          {/* <Nav /> */}
 
-      <Routes>
-        <Route path="/" element={<RequireAuth>
-          
-          <Flex height={height}>
-            <Box width={"30%"}>
-              <Home/>
-            </Box>
-            <Box width={"70%"} sx={{bg:'#F2F2F2'}}>
-            <Chat/>
-            </Box>
-          </Flex>
-        
-        </RequireAuth>} />
-        <Route path="/chat/*" element={<RequireAuth>
-          <Flex  height={height}>
-            <Box width={"30%"}>
-              <Home/>
-            </Box>
-            <Box width={"70%"} sx={{bg:'#F2F2F2'}}>
-            <Chat/>
-            </Box>
-          </Flex>
-          
-          </RequireAuth>} />
-        <Route path="/login" element={<Login />}/>
-      </Routes>
-
-    </div>
-    </BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Flex height={height}>
+                    <Box width={"30%"}>
+                      <SideNav />
+                    </Box>
+                    <Box width={"70%"} sx={{ bg: "#F2F2F2" }}>
+                      <Chat />
+                    </Box>
+                  </Flex>
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/chat/*"
+              element={
+                <RequireAuth>
+                  <Flex height={height}>
+                    <Box width={"30%"}>
+                      <SideNav />
+                    </Box>
+                    <Box width={"70%"} sx={{ bg: "#F2F2F2" }}>
+                      <Chat />
+                    </Box>
+                  </Flex>
+                </RequireAuth>
+              }
+            />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,52 +4,52 @@ const AuthContext = React.createContext();
 
 function useAuth() {
   const [authed, setAuthed] = React.useState(false);
-  const [data,setData] = React.useState([])
-  const [chat,setChat] = React.useState([])
+  const [data, setData] = React.useState([]);
+  const [chat, setChat] = React.useState([]);
   return {
     authed,
     data,
     chat,
-    setNewChat(newData){
-      let tempChat = chat
+    setNewChat(newData) {
+      let tempChat = chat;
       return new Promise((res) => {
-        tempChat[0].transcript.push(newData)
-        setChat(tempChat)  
+        tempChat[0].transcript.push(newData);
+        setChat(tempChat);
         res();
       });
     },
-    setNewData(id,newData){
-      let tempData = data
-      return new Promise((res)=>{
-       let index =  tempData.findIndex((d)=>d.id == id)
-       tempData[index].transcript.push(newData)
-       tempData[index].lastmessage = newData
-       setData(tempData)
-       res();
-      })
-    },
-    fetchData(){
-      console.log('runnin')
+    setNewData(id, newData) {
+      let tempData = data;
       return new Promise((res) => {
-        const createData =  createChatData()
-        setData(createData)  
+        let index = tempData.findIndex((d) => d.id == id);
+        tempData[index].transcript.push(newData);
+        tempData[index].lastmessage = newData;
+        setData(tempData);
+        res();
+      });
+    },
+    fetchData() {
+      console.log("runnin");
+      return new Promise((res) => {
+        const createData = createChatData();
+        setData(createData);
         res();
       });
     },
 
-    getChat(id){
-      console.log(id)
+    getChat(id) {
+      console.log(id);
       return new Promise((res) => {
-        const selectedChat = data.filter((d)=>d.id == id)
-        setChat(selectedChat)  
+        const selectedChat = data.filter((d) => d.id == id);
+        setChat(selectedChat);
         res();
-      }); 
+      });
     },
-   
+
     login() {
-      console.log('called')
+      console.log("called");
       return new Promise((res) => {
-        console.log('resloved')
+        console.log("resloved");
         setAuthed(true);
         res();
       });
@@ -59,19 +59,17 @@ function useAuth() {
         setAuthed(false);
         res();
       });
-    }
+    },
   };
 }
 
 export function AuthProvider({ children }) {
   const auth = useAuth();
 
-  return (
-    <AuthContext.Provider value={auth}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 }
+
+//hook
 
 export default function AuthConsumer() {
   return React.useContext(AuthContext);
